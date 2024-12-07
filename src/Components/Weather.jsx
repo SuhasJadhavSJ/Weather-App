6ab26284ad6e65b34cf660daf6b7c868dffdc556
SuhasJadhavSJ <suhasjadhavsj046@gmail.com>
2024-12-07 18:23:27 +0530

    done

--- src/Components/Weather.jsx
+++ src/Components/Weather.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import './Weather.css'
 import searchIcon from '../assets/search.png'
+import cloud from '../assets/cloud.png'
+import clear from '../assets/clear.png'
+import drizzle from '../assets/drizzle.png'
+import humidity from '../assets/humidity.png'
+import rain from '../assets/rain.png'
+import snow from '../assets/snow.png'
+import wind from '../assets/wind.png'
 
 const Weather = () => {
   return (
@@ -9,6 +16,7 @@ const Weather = () => {
         <input type="text" placeholder='search'/>
         <img src={searchIcon} alt="" />
       </div>
+      <img src="" alt="" />
     </div>
   )
 }
